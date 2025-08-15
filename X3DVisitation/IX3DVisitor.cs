a7196e2abf5583acb60991223e0ecf02a351fb2a
Jonas Helmer <Jonas.Helmer@tarakos.de>
2025-08-15 09:12:40 +0200

    WIP: Visitor Interface

---
+++ X3DVisitation/IX3DVisitor.cs
@@ -0,0 +1,19 @@
+using Free.FileFormats.VRML.Nodes;
+
+namespace Free.FileFormats.VRML.X3DVisitation;
+
+public interface IX3DVisitor
+{
+    void Visit(Scene scene);
+    void Visit(x3dGroup group);
+    void Visit(x3dTransform transform);
+    void Visit(x3dShape shape);
+    void Visit(x3dBox box);
+    void Visit(x3dCone cone);
+    void Visit(x3dCylinder cylinder);
+    void Visit(x3dElevationGrid elevationGrid);
+    void Visit(x3dExtrusion extrusion);
+    void Visit(x3dIndexedFaceSet indexedFaceSet);
+    //void Visit(x3dViewpoint viewpoint);
+    void Visit(IX3DVisitable node);
+}
